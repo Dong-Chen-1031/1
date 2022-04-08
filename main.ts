c2fff755d178cf09135252dc31ea26189a0390f1
Dong-Chen-1031 <90507781+Dong-Chen-1031@users.noreply.github.com>
2022-04-08 20:33:01 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,22 +3,55 @@ radio.onReceivedValue(function (name, value) {
         soundExpression.giggle.playUntilDone()
     }
 })
+function 關窗戶 () {
+    for (let index = 0; index < 4; index++) {
+        images.createImage(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `).scrollImage(-1, 200)
+    }
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    窗戶開著 = false
+}
+function 開窗戶 () {
+    for (let index = 0; index < 4; index++) {
+        images.createImage(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `).scrollImage(1, 200)
+    }
+    窗戶開著 = true
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+}
+let 窗戶開著 = false
 radio.setGroup(4)
+窗戶開著 = true
 basic.forever(function () {
     if (input.soundLevel() > 120) {
-        for (let index = 0; index < 4; index++) {
-            basic.showLeds(`
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                # # # # #
-                `)
-            basic.pause(200)
-            basic.clearScreen()
-            basic.pause(200)
+        if (窗戶開著) {
+            關窗戶()
         }
-    } else {
-        basic.clearScreen()
+    } else if (input.soundLevel() < 100) {
+        if (!(窗戶開著)) {
+            開窗戶()
+        }
     }
 })
